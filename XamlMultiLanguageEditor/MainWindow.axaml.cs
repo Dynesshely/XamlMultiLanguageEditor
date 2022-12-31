26cf8dcd7a7e5f590aedb44df3274e9cf9a0d233
Dynesshely <catrol@qq.com>
2022-12-31 22:52:34 +0800

    💾 📝 📦 Feat, Chore, Struct: Init project

---
+++ XamlMultiLanguageEditor/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace XamlMultiLanguageEditor
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
