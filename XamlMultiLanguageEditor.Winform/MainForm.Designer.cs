fbdf7b8a85af40fe93ae0a53f133c8c0876ef138
Dynesshely <catrol@qq.com>
2023-01-02 02:18:43 +0800

    💾 📦 Feat, Struct: 加载, 列出键与值, 编辑某个值, 格式化保存

--- XamlMultiLanguageEditor.Winform/MainForm.Designer.cs
+++ XamlMultiLanguageEditor.Winform/MainForm.Designer.cs
@@ -28,13 +28,152 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button_load = new System.Windows.Forms.Button();
+            this.textBox_path = new System.Windows.Forms.TextBox();
+            this.listBox_keys = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox_values = new System.Windows.Forms.ListBox();
+            this.textBox_value = new System.Windows.Forms.TextBox();
+            this.button_save_value = new System.Windows.Forms.Button();
+            this.textBox_key = new System.Windows.Forms.TextBox();
+            this.button_insert_key = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_load
+            // 
+            this.button_load.Location = new System.Drawing.Point(12, 12);
+            this.button_load.Name = "button_load";
+            this.button_load.Size = new System.Drawing.Size(75, 23);
+            this.button_load.TabIndex = 0;
+            this.button_load.Text = "Load";
+            this.button_load.UseVisualStyleBackColor = true;
+            this.button_load.Click += new System.EventHandler(this.Button_load_Click);
+            // 
+            // textBox_path
+            // 
+            this.textBox_path.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_path.Location = new System.Drawing.Point(93, 12);
+            this.textBox_path.Name = "textBox_path";
+            this.textBox_path.Size = new System.Drawing.Size(679, 23);
+            this.textBox_path.TabIndex = 1;
+            // 
+            // listBox_keys
+            // 
+            this.listBox_keys.FormattingEnabled = true;
+            this.listBox_keys.HorizontalScrollbar = true;
+            this.listBox_keys.ItemHeight = 17;
+            this.listBox_keys.Location = new System.Drawing.Point(12, 58);
+            this.listBox_keys.Name = "listBox_keys";
+            this.listBox_keys.Size = new System.Drawing.Size(300, 395);
+            this.listBox_keys.TabIndex = 2;
+            this.listBox_keys.SelectedIndexChanged += new System.EventHandler(this.ListBox_keys_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Keys";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(318, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Values";
+            // 
+            // listBox_values
+            // 
+            this.listBox_values.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox_values.FormattingEnabled = true;
+            this.listBox_values.HorizontalScrollbar = true;
+            this.listBox_values.ItemHeight = 17;
+            this.listBox_values.Location = new System.Drawing.Point(318, 58);
+            this.listBox_values.Name = "listBox_values";
+            this.listBox_values.Size = new System.Drawing.Size(454, 395);
+            this.listBox_values.TabIndex = 4;
+            this.listBox_values.SelectedIndexChanged += new System.EventHandler(this.ListBox_values_SelectedIndexChanged);
+            // 
+            // textBox_value
+            // 
+            this.textBox_value.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_value.Location = new System.Drawing.Point(318, 459);
+            this.textBox_value.Name = "textBox_value";
+            this.textBox_value.Size = new System.Drawing.Size(373, 23);
+            this.textBox_value.TabIndex = 6;
+            // 
+            // button_save_value
+            // 
+            this.button_save_value.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_save_value.Location = new System.Drawing.Point(697, 459);
+            this.button_save_value.Name = "button_save_value";
+            this.button_save_value.Size = new System.Drawing.Size(75, 23);
+            this.button_save_value.TabIndex = 7;
+            this.button_save_value.Text = "Save";
+            this.button_save_value.UseVisualStyleBackColor = true;
+            this.button_save_value.Click += new System.EventHandler(this.Button_save_value_Click);
+            // 
+            // textBox_key
+            // 
+            this.textBox_key.Location = new System.Drawing.Point(12, 459);
+            this.textBox_key.Name = "textBox_key";
+            this.textBox_key.Size = new System.Drawing.Size(199, 23);
+            this.textBox_key.TabIndex = 8;
+            // 
+            // button_insert_key
+            // 
+            this.button_insert_key.Location = new System.Drawing.Point(217, 459);
+            this.button_insert_key.Name = "button_insert_key";
+            this.button_insert_key.Size = new System.Drawing.Size(95, 23);
+            this.button_insert_key.TabIndex = 9;
+            this.button_insert_key.Text = "Insert Key";
+            this.button_insert_key.UseVisualStyleBackColor = true;
+            this.button_insert_key.Click += new System.EventHandler(this.button_insert_key_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.button_insert_key);
+            this.Controls.Add(this.textBox_key);
+            this.Controls.Add(this.button_save_value);
+            this.Controls.Add(this.textBox_value);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox_values);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox_keys);
+            this.Controls.Add(this.textBox_path);
+            this.Controls.Add(this.button_load);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "XamlMultiLanguageEditor.Winform";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button_load;
+        private System.Windows.Forms.TextBox textBox_path;
+        private System.Windows.Forms.ListBox listBox_keys;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox_values;
+        private System.Windows.Forms.TextBox textBox_value;
+        private System.Windows.Forms.Button button_save_value;
+        private System.Windows.Forms.TextBox textBox_key;
+        private System.Windows.Forms.Button button_insert_key;
     }
 }
 
--- XamlMultiLanguageEditor.Winform/MainForm.cs
+++ XamlMultiLanguageEditor.Winform/MainForm.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace XamlMultiLanguageEditor.Winform
 {
@@ -17,5 +19,163 @@ namespace XamlMultiLanguageEditor.Winform
             InitializeComponent();
         }
 
+        private readonly Dictionary<string, XmlDocument> _languages = new();
+        private readonly Dictionary<string, string> _paths = new();
+        private readonly Dictionary<string, Dictionary<string, string>> _langs
+            = new();
+
+        private void Button_load_Click(object sender, EventArgs e)
+        {
+            _languages.Clear();
+            _langs.Clear();
+            _paths.Clear();
+
+            LoadFromDisk();
+
+            RestoreUI();
+            DrawUI();
+        }
+
+        private void LoadFromDisk()
+        {
+            var path = textBox_path.Text;
+            if (path is null) return;
+            path = Path.GetFullPath(path);
+            if (!Directory.Exists(path)) return;
+            var dir = new DirectoryInfo(path);
+            var files = dir.GetFiles();
+            foreach (var file in files)
+            {
+                var key = Path.GetFileNameWithoutExtension(file.FullName);
+                _paths.Add(key, file.FullName);
+                _languages.Add(key, new XmlDocument());
+                _languages[key].Load(file.FullName);
+            }
+
+            LoadFromXmlDocuments();
+        }
+
+        private void LoadFromXmlDocuments()
+        {
+            var index = 0;
+            foreach (var item in _languages)
+            {
+                var nodes = item.Value.DocumentElement?.ChildNodes;
+                if (nodes is null) continue;
+                foreach (XmlNode? node in nodes)
+                {
+                    try
+                    {
+                        var key = node?.Attributes?["x:Key"]?.InnerText;
+                        if (key is null) continue;
+                        if (index == 0) _langs.Add(key, new Dictionary<string, string>());
+                        _langs[key][item.Key] = node?.InnerText ?? string.Empty;
+                    }
+                    catch (Exception ex)
+                    {
+                        // ignored
+                    }
+                }
+
+                ++index;
+            }
+        }
+
+        private void DrawUI()
+        {
+            listBox_keys.Items.Clear();
+            foreach (var item in _langs)
+            {
+                listBox_keys.Items.Add(item.Key);
+            }
+        }
+
+        private void RestoreUI()
+        {
+            listBox_keys.Items.Clear();
+            listBox_values.Items.Clear();
+        }
+
+        private void ListBox_keys_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadValues();
+        }
+
+        private void LoadValues()
+        {
+            listBox_values.Items.Clear();
+            var values = _langs[listBox_keys.SelectedItem as string];
+            foreach (var item in values)
+            {
+                listBox_values.Items.Add($"{item.Key}:\t{item.Value}");
+            }
+        }
+
+        private void ListBox_values_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox_value.Text = RemoveLangKey(listBox_values.SelectedItem as string);
+        }
+
+        private string RemoveLangKey(string str)
+        {
+            foreach (var key in _languages.Keys)
+                if (str.StartsWith(key))
+                {
+                    return str.Replace($"{key}:\t", "");
+                }
+            return string.Empty;
+        }
+
+        private string RemoveLangValue(string str)
+        {
+            foreach (var key in _languages.Keys)
+                if (str.StartsWith(key)) return key;
+            return string.Empty;
+        }
+
+        private void Button_save_value_Click(object sender, EventArgs e)
+        {
+            if (listBox_values.SelectedIndex >= 0)
+            {
+                var key = _languages.Keys.ToList()[listBox_values.SelectedIndex];
+                var doc = _languages[key];
+                var nodes = doc.DocumentElement?.ChildNodes;
+                foreach (XmlNode node in nodes)
+                {
+                    if (node.Attributes["x:Key"].InnerText.Equals(listBox_keys.SelectedItem as string))
+                    {
+                        node.InnerText = textBox_value.Text;
+                        break;
+                    }
+                }
+                //doc.Save(_paths[key]);
+                SaveXml(key, ref doc);
+
+                _langs[listBox_keys.SelectedItem as string][key] = textBox_value.Text;
+                var nowIndex = listBox_values.SelectedIndex;
+                LoadValues();
+                listBox_values.SelectedIndex = nowIndex;
+            }
+        }
+
+        private void SaveXml(string key, ref XmlDocument doc)
+        {
+            using var xw = XmlWriter.Create(_paths[key], new()
+            {
+                Indent = true,
+                Encoding = Encoding.UTF8,
+                IndentChars = "    ",
+                OmitXmlDeclaration = true,
+            });
+            doc.WriteTo(xw);
+        }
+
+        private void button_insert_key_Click(object sender, EventArgs e)
+        {
+            if (listBox_keys.SelectedIndex >= 0)
+            {
+
+            }
+        }
     }
 }
